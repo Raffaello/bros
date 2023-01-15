5757d5e02c410fd7c981e6011232daa41da8758c
Raffaello Bertini <raffaellobertini@gmail.com>
2023-01-15 02:39:00 +0000

    refactor

--- bootloader/filesystems/FAT12.asm
+++ bootloader/filesystems/FAT12.asm
@@ -3,6 +3,17 @@
 .ifndef __FAT12__
 __FAT12__:
 
-.func 
 
+# ************************************* #
+# Load Root Directory of FAT12/16       #
+# ------------------------------------- #
+# Parameters:                           #
+#
+# Returns:                              #
+#
+# ************************************* #
+.func LoadRootDirectory
+LoadRootDirectory:
+  
+.endfunc
 .endif
