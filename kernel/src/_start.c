13a7dca59fcf9ea0bf47092e846240655296e3b0
Raffaello Bertini <r.bertini@setsnc.com>
2026-06-26 17:35:40 +0200

    code rev

--- kernel/src/_start.c
+++ kernel/src/_start.c
@@ -121,7 +121,7 @@ _start_init()
     CON_puts("Init IRQ\n");
     IRQ_init();
     CON_puts("Init PIT\n");
-    PIT_init(1);
+    PIT_init(10);
     CON_puts("Init PS/2\n");
     PS2_init();
     // boot info sanitize
